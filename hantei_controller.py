aeb1465c4c5d43c686632a93402de33f5c8689a5
fummicc1 <fumiya.tennis1@gmail.com>
2021-10-28 23:51:30 +0900

    :sparkles: Read custom model

--- hantei_controller.py
+++ hantei_controller.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 # 判定の処理を行うクラス
 class HanteiController:
@@ -9,10 +10,13 @@ class HanteiController:
     # クラス名とIDの組み合わせ
     class_name = None
     LABEL2CLS = {0: "rock", 1: "scissors", 2: "paper"}
+    model: any
+
+    def __init__(self):
+        self.model = torch.hub.load('.', 'custom', path='best.pt', source='local')
 
     def judge(self, frame: np.ndarray) -> str:
-        model =
-        model.conf = 0.5  # minimum confidence threshold
-        results = model(frame, size=240)
+        self.model.conf = 0.5  # minimum confidence threshold
+        results = self.model(frame, size=240)
         print(results)
         return ""
